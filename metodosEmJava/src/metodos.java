34093555a4587be6ee5523898285998fbd257f70
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2024-08-11 07:10:52 -0300

    aula sobre metodos

--- metodosEmJava/src/metodos.java
+++ metodosEmJava/src/metodos.java
@@ -2,4 +2,9 @@ public class metodos {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
-}
+    // classe smartTv
+    // classes e metodos : 
+    // statusTv (ON/OFF) tipo : boolean /  nome do metodo : ligarEdesligar
+    // botaoVolume (+/-) tipo : int /  nome do metodo : aumentarEdiminuir (+1 ou -1)
+    // canaisDaTv (+/-) tipo : int /  nome do metodo : mudarCanal (de 1 em 1 ou número correspondente)
+    
