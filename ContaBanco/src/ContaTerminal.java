101a0b849f451714b40d627b0ab8fcf4eda55716
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2024-08-11 18:28:42 -0300

    desafio01 Java Dio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,13 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+// TODO: relizara as perguntas para o usuário
+
+// 
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ ContaBanco/src/contaBancaria.java
@@ -0,0 +1,12 @@
+
+
+
+
+public class contaBancaria {
+
+    double saldo = 6500;
+    String cliente = " ";
+    String numeroAgencia = "067-8";
+    Int numeroUsuario = 1021; 
+    
+}
